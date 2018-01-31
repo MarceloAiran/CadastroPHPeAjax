27caa3da36b234ec6a915d950dfc973a8f2599b8
MarceloAiran <marcelo.airan@gmail.com>
2018-01-31 13:58:59 -0200

    Add files via upload

---
+++ conecta.php
@@ -0,0 +1,13 @@
+<?php 
+	$conect	= mysqli_connect("Localhost", "root" ,'' );
+   
+
+    // Caso a conexão seja reprovada, exibe na tela uma mensagem de erro
+	if (!$conect) die ("<h1>Falha na conexão com o Banco de Dados!</h1>");
+	// Caso a conexão seja aprovada, então conecta o Banco de Dados.
+	$db = mysql_select_db("meuBanco");
+	/*Configurando este arquivo, depois é só você dar um include em suas paginas php,
+isto facilita muito, pois caso haja necessidade de mudar seu Banco de Dados
+você altera somente um arquivo*/
+
+?>	
